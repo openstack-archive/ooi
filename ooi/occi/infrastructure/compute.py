e7a0477fc7d8645b2876d8faf4e16f99a1f7d4e9
Alvaro Lopez Garcia <aloga@ifca.unican.es>
2015-03-03 10:48:36 +0100

    initial implementation of OCCI Compute resource

---
+++ ooi/occi/infrastructure/compute.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2015 Spanish National Research Council
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from ooi.occi.core import action
+from ooi.occi.core import attribute as attr
+from ooi.occi.core import kind
+from ooi.occi.core import resource
+from ooi.occi import helpers
+
+start = action.Action(helpers.build_schema('infrastructure/compute/action'),
+                      "start", "start compute instance")
+
+stop = action.Action(helpers.build_schema('infrastructure/compute/action'),
+                     "stop", "stop compute instance")
+
+restart = action.Action(helpers.build_schema('infrastructure/compute/action'),
+                        "restart", "restart compute instance")
+
+suspend = action.Action(helpers.build_schema('infrastructure/compute/action'),
+                        "suspend", "suspend compute instance")
+
+
+class ComputeResource(resource.Resource):
+    attributes = attr.AttributeCollection(["occi.compute.architecture",
+                                           "occi.compute.cores",
+                                           "occi.compute.hostname",
+                                           "occi.compute.speed",
+                                           "occi.compute.memory",
+                                           "occi.compute.state"])
+    actions = (start, stop, restart, suspend)
+    kind = kind.Kind(helpers.build_schema('infrastructure'), 'compute',
+                     'compute resource', attributes, '/compute/',
+                     actions=actions)
+
+    def __init__(self, title, summary=None, id=None, architecture=None,
+                 cores=None, hostname=None, speed=None, memory=None,
+                 state=None):
+
+        mixins = []
+
+        super(ComputeResource, self).__init__(title, mixins, summary=summary,
+                                              id=id)
+
+        self.attributes["occi.compute.architecture"] = attr.MutableAttribute(
+            "occi.compute.architecture", architecture)
+        self.attributes["occi.compute.cores"] = attr.MutableAttribute(
+            "occi.compute.cores", cores)
+        self.attributes["occi.compute.hostname"] = attr.MutableAttribute(
+            "occi.compute.hostname", hostname)
+        self.attributes["occi.compute.speed"] = attr.MutableAttribute(
+            "occi.compute.speed", speed)
+        self.attributes["occi.compute.memory"] = attr.MutableAttribute(
+            "occi.compute.memory", memory)
+        self.attributes["occi.compute.state"] = attr.InmutableAttribute(
+            "occi.compute.state", state)
+
+    @property
+    def architecture(self):
+        return self.attributes["occi.compute.architecture"].value
+
+    @architecture.setter
+    def architecture(self, value):
+        self.attributes["occi.compute.architecture"].value = value
+
+    @property
+    def cores(self):
+        return self.attributes["occi.compute.cores"].value
+
+    @cores.setter
+    def cores(self, value):
+        self.attributes["occi.compute.cores"].value = value
+
+    @property
+    def hostname(self):
+        return self.attributes["occi.compute.hostname"].value
+
+    @hostname.setter
+    def hostname(self, value):
+        self.attributes["occi.compute.hostname"].value = value
+
+    @property
+    def speed(self):
+        return self.attributes["occi.compute.speed"].value
+
+    @speed.setter
+    def speed(self, value):
+        self.attributes["occi.compute.speed.speed"].value = value
+
+    @property
+    def memory(self):
+        return self.attributes["occi.compute.memory"].value
+
+    @memory.setter
+    def memory(self, value):
+        self.attributes["occi.compute.memory"].value = value
+
+    @property
+    def state(self):
+        return self.attributes["occi.compute.state"].value
---
+++ ooi/tests/test_occi_infrastructure.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2015 Spanish National Research Council
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import uuid
+
+from ooi.occi.infrastructure import compute
+from ooi.tests import base
+
+
+class TestOCCICompute(base.TestCase):
+    def test_compute_class(self):
+        c = compute.ComputeResource
+        self.assertIn(compute.start, c.actions)
+        self.assertIn(compute.stop, c.actions)
+        self.assertIn(compute.suspend, c.actions)
+        self.assertIn(compute.restart, c.actions)
+        self.assertIn("occi.core.id", c.attributes)
+        self.assertIn("occi.core.summary", c.attributes)
+        self.assertIn("occi.core.title", c.attributes)
+        self.assertIn("occi.compute.architecture", c.attributes)
+        self.assertIn("occi.compute.cores", c.attributes)
+        self.assertIn("occi.compute.hostname", c.attributes)
+        self.assertIn("occi.compute.memory", c.attributes)
+        self.assertIn("occi.compute.speed", c.attributes)
+        self.assertIn("occi.compute.state", c.attributes)
+        # TODO(aloga): We need to check that the attributes are actually set
+        # after we get an object (we have to check this for this but also for
+        # the other resources)
+
+    def test_compute(self):
+        id = uuid.uuid4().hex
+        c = compute.ComputeResource("foo",
+                                    summary="This is a summary",
+                                    id=id)
+        self.assertEqual("foo", c.title)
+        self.assertEqual(id, c.id)
+        self.assertEqual("This is a summary", c.summary)
+        self.assertIsNone(c.architecture)
+        self.assertIsNone(c.cores)
+        self.assertIsNone(c.hostname)
+        self.assertIsNone(c.memory)
+        self.assertIsNone(c.speed)
