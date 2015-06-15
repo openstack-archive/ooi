0620bd6881e611d296a89aa98bbee6e988fe2a2b
Alvaro Lopez Garcia <aloga@ifca.unican.es>
2015-06-15 17:03:52 +0200

    refactor 'delete' method for compute controller
    Adapt it so that it uses the newly introduced OpenStack helper.
    
    Change-Id: Id5e866e86307117d4121e0a30af82262035eb48e

---
+++ ooi/tests/controllers/test_compute.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2015 Spanish National Research Council
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import uuid
+
+import mock
+import webob
+
+from ooi.api import compute
+from ooi.tests.controllers import base
+from ooi.tests import fakes
+
+
+class FakeException(Exception):
+    pass
+
+
+class TestComputeController(base.TestController):
+    def setUp(self):
+        super(TestComputeController, self).setUp()
+        self.controller = compute.Controller(mock.MagicMock(), None)
+
+    def _build_req(self, tenant_id, **kwargs):
+        m = mock.MagicMock()
+        m.user.project_id = tenant_id
+        environ = {"keystone.token_auth": m}
+
+        kwargs["base_url"] = self.application_url
+
+        return webob.Request.blank("/whatever", environ=environ, **kwargs)
+
+    @mock.patch("webob.Request.get_response")
+    def test_index(self, m_get_response):
+        tenant = fakes.tenants["foo"]
+        req = self._build_req(tenant["id"])
+
+        test_servers = [
+            [],
+            fakes.servers[fakes.tenants["foo"]["id"]]
+        ]
+
+        for servers in test_servers:
+            resp_data = {"servers": servers}
+            response = fakes.create_fake_json_resp(data=resp_data, status=200)
+            m_get_response.return_value = response
+            result = self.controller.index(req)
+            expected = self.controller._get_compute_resources(servers)
+            self.assertItemsEqual(expected, result.resources)
+
+    def test_delete_ids(self):
+        tenant = fakes.tenants["foo"]
+        server_uuids = [uuid.uuid4().hex, uuid.uuid4().hex]
+        req = self._build_req(tenant["id"])
+        response = fakes.create_fake_json_resp({}, 204)
+        with mock.patch("webob.Request.get_response") as m_get_response:
+            m_get_response.return_value = response
+            self.controller._delete(req, server_uuids)
+
+    @mock.patch("ooi.api.base.exception_from_response")
+    @mock.patch("webob.Request.get_response")
+    def test_delete_ids_with_failure(self, m_get_response, m_exc):
+        tenant = fakes.tenants["foo"]
+        server_uuids = [uuid.uuid4().hex, uuid.uuid4().hex]
+        req = self._build_req(tenant["id"])
+        response = fakes.create_fake_json_resp({}, 500)
+
+        m_get_response.return_value = response
+        m_exc.return_value = FakeException()
+        self.assertRaises(FakeException,
+                          self.controller._delete,
+                          req,
+                          server_uuids)
+
+    @mock.patch.object(compute.Controller, "_delete")
+    def test_delete(self, m_delete):
+        tenant = fakes.tenants["foo"]
+        req = self._build_req(tenant["id"])
+        server_uuid = uuid.uuid4().hex
+        m_delete.return_value = []
+        ret = self.controller.delete(req, server_uuid)
+        m_delete.assert_called_with(req, [server_uuid])
+        self.assertEqual([], ret)
+
+    @mock.patch.object(compute.OpenStackHelper, "index")
+    @mock.patch.object(compute.Controller, "_delete")
+    def test_delete_all(self, m_delete, m_helper_index):
+        tenant = fakes.tenants["foo"]
+        req = self._build_req(tenant["id"])
+        servers = [{"id": uuid.uuid4().hex}, {"id": uuid.uuid4().hex}]
+        m_helper_index.return_value = servers
+        m_delete.return_value = []
+        ret = self.controller.delete_all(req)
+        m_delete.assert_called_with(req, [s["id"] for s in servers])
+        self.assertEqual([], ret)
