bc980d0477d365cbc9645da77f900d4c86f4924e
Alvaro Lopez Garcia <aloga@ifca.unican.es>
2015-06-15 17:03:52 +0200

    refactor 'delete' method for compute controller
    Adapt it so that it uses the newly introduced OpenStack helper.
    
    Change-Id: Id5e866e86307117d4121e0a30af82262035eb48e

---
+++ ooi/tests/controllers/test_compute.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2015 Spanish National Research Council
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import uuid
+
+import mock
+import webob
+
+from ooi.api import compute
+from ooi.api import helpers
+from ooi.tests.controllers import base
+
+
+class FakeException(Exception):
+    pass
+
+
+class TestComputeController(base.TestController):
+    def setUp(self):
+        super(TestComputeController, self).setUp()
+        self.controller = compute.Controller(mock.MagicMock(), None)
+
+    def _build_req(self, tenant_id, **kwargs):
+        m = mock.MagicMock()
+        m.user.project_id = tenant_id
+        environ = {"keystone.token_auth": m}
+
+        kwargs["base_url"] = self.application_url
+
+        return webob.Request.blank("/whatever", environ=environ, **kwargs)
+
+    @mock.patch.object(compute.Controller, "_delete")
+    def test_delete(self, mock_delete):
+        mock_delete.return_value = []
+        ret = self.controller.delete(None, "foo")
+        self.assertEqual([], ret)
+        mock_delete.assert_called_with(None, ["foo"])
+
+    @mock.patch.object(helpers.OpenStackHelper, "delete")
+    def test_delete_ids(self, mock_delete):
+        server_ids = [uuid.uuid4().hex, uuid.uuid4().hex]
+        mock_delete.return_value = None
+        ret = self.controller._delete(None, server_ids)
+        self.assertEqual([], ret)
+        mock_delete.assert_has_calls([mock.call(None, s) for s in server_ids])
+
+    @mock.patch.object(helpers.OpenStackHelper, "index")
+    @mock.patch.object(compute.Controller, "_delete")
+    def test_delete_all(self, mock_delete, mock_index):
+        servers = [{"id": uuid.uuid4().hex}, {"id": uuid.uuid4().hex}]
+        mock_index.return_value = servers
+        mock_delete.return_value = []
+        ret = self.controller.delete_all(None)
+        self.assertEqual([], ret)
+        mock_delete.assert_called_with(None, [s["id"] for s in servers])
