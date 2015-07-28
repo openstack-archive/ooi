30556fbba9c1e4a40356c8a63430211706443b2a
Alvaro Lopez Garcia <aloga@ifca.unican.es>
2015-07-28 11:08:23 +0200

    helpers: added a base class and openstack API helper
    Part of the controller refactoring.
    
    Introduce a new class 'OpenStackHelper' that will be used for making the
    correct requests for the nova API and handlign the responses. This way
    we can test individually each of the expected requests, and it will be
    easier to switch from one version of the nova API to another by just
    switching the helper class.
    
    Change-Id: I5c80936a66c0121878dcefd42e3a92ed045e6a0c

---
+++ ooi/tests/controllers/base.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2015 Spanish National Research Council
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from ooi.tests import base
+from ooi.tests import fakes
+
+
+class TestController(base.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(TestController, self).__init__(*args, **kwargs)
+
+    def setUp(self):
+        super(TestController, self).setUp()
+        self.application_url = fakes.application_url
+
+    def assertExpectedReq(self, method, path, body, request):
+        self.assertEqual(method, request.method)
+        self.assertEqual(path, request.path_info)
+        if body and request.content_type == "application/json":
+            self.assertDictEqual(body, request.json_body)
+        else:
+            self.assertEqual(body, request.text)
