631d912561e3d5a6daa6c7a77ee3c2c87889005b
jorgesece <jorgesece@lip.pt>
2016-06-03 17:12:25 +0100

    Add mixin to indicate that neutron is being used
    We add a mixin with schema and term descriptive to indicatest
    that neutron component is being used for network management.
    This information is showed in the OCCI discovery functionality.
    We add a test to test it.
    
    Change-Id: I3dbb79b2533c909074341cc0d9f8ba3708a76f89

--- ooi/api/query.py
+++ ooi/api/query.py
@@ -31,8 +31,12 @@ from ooi.openstack import templates
 
 
 class Controller(base.Controller):
-    def __init__(self, *args, **kwargs):
-        super(Controller, self).__init__(*args, **kwargs)
+    def __init__(self, app, openstack_version,
+                 neutron_ooi_endpoint=None):
+        self.neutron_ooi_endpoint = neutron_ooi_endpoint
+        super(Controller, self).__init__(
+            app=app,
+            openstack_version=openstack_version)
         self.os_helper = ooi.api.helpers.OpenStackHelper(
             self.app,
             self.openstack_version
@@ -87,6 +91,8 @@ class Controller(base.Controller):
         # OCCI infra network
         l.append(network.NetworkResource.kind)
         l.extend(network.NetworkResource.actions)
+        if self.neutron_ooi_endpoint:
+            l.append(os_network.neutron_network)
         l.append(network.ip_network)
         l.append(network_link.NetworkInterface.kind)
         l.append(network_link.ip_network_interface)
