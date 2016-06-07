61d99a666753512b07565b3f46ce9c3b5635388c
Enol Fernandez <enolfc@ifca.unican.es>
2016-06-07 16:01:19 +0200

    Fix network allocation parameter
    Closes-Bug: 1590018
    Change-Id: Ib865c86a87c9799e9133b080bafd44059ea60e79

--- ooi/openstack/network.py
+++ ooi/openstack/network.py
@@ -155,11 +155,11 @@ class OSNetworkResource(network.NetworkResource):
 
     @property
     def allocation(self):
-        return self.attributes["occi.network.network.allocation"].value
+        return self.attributes["occi.network.allocation"].value
 
     @allocation.setter
     def allocation(self, value):
-        self.attributes["occi.network.network.allocation"] = value
+        self.attributes["occi.network.allocation"].value = value
 
 
 neutron_network = mixin.Mixin(helpers.build_scheme("infrastructure/network"),
